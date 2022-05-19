1be87eb07198482412d6d3699844fcf604476c06
shorelinedigital <104526348+shorelinedigital@users.noreply.github.com>
2022-05-18 18:26:53 -0700

    v2

---
+++ index.js
@@ -0,0 +1,10 @@
+const helloBtn = document.getElementById("hello-btn")
+const messageEl = document.getElementById("message-el")
+
+helloBtn.addEventListener("click", function(){
+   
+    messageEl.textContent += "Hello World "
+
+    
+})
+
