6b1fb342313981b346cd648c3178ed39bf6a99ef
shorelinedigital <104526348+shorelinedigital@users.noreply.github.com>
2022-05-24 21:45:36 -0700

    popup-finished

---
+++ popup.js
@@ -0,0 +1,16 @@
+document.getElementById("buy").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = 'flex';
+
+
+})
+
+document.getElementById("buy-2").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = 'flex';
+
+ 
+})
+
+document.querySelector(".close").addEventListener("click", function(){
+document.querySelector(".popup").style.display = 'none';
+
+})
