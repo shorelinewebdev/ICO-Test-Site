e81310a87a8f87522bb05f4d2df58c04041cd3a5
Alexander Gifford <104526348+shorelinedigital@users.noreply.github.com>
2022-05-30 15:54:20 -0700

    2022-05-30
    Before modification

---
+++ addToMetaMask.js
@@ -0,0 +1,31 @@
+const tokenAddress = '0x1a0E6664D47218F2a31Ea3220A834EDF98da5884';
+const tokenSymbol = 'xNOVA';
+const tokenDecimals = 0;
+
+
+async function addTokenFunction() {
+
+try {
+  
+  const wasAdded = await ethereum.request({
+    method: 'wallet_watchAsset',
+    params: {
+      type: 'ERC20', 
+      options: {
+        address: tokenAddress, 
+        symbol: tokenSymbol, 
+        decimals: tokenDecimals, 
+         
+      },
+    },
+  });
+
+  if (wasAdded) {
+    console.log('Thanks for your interest!');
+  } else {
+    console.log('HelloWorld Coin has not been added');
+  }
+} catch (error) {
+  console.log(error);
+}
+}
--- web3app.js
+++ web3app.js
@@ -1,33 +1,284 @@
 
+var web3;
+    
+async function connect() {
+
+    if (window.ethereum) {
+       await window.ethereum.send('eth_requestAccounts');
+       window.web3 = new Web3(window.ethereum);
+    }
+}
 
 
 
-if (typeof window.ethereum !== 'undefined') {
-    console.log('MetaMask is installed!');
-  } else {
-      conspole.log('anger anger')
-  }
+//check for metamask
 
 
-  const ethereumButton = document.querySelector('.connect-btn');
-  const showAccount = document.querySelector('.showAccount');
-
-  ethereumButton.addEventListener('click', () => {
-    //Will Start the metamask extension
-    ethereum.request({ method: 'eth_requestAccounts' });
-  });
-
-  ethereumButton.addEventListener('click', () => {
-    getAccount();
-  });
+  if (typeof web3 !== 'undefined') {
+    web3 = new Web3(ethereum);
+    console.log(web3.eth.accounts);
   
-  async function getAccount() {
-    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-    const account = accounts[0];
-    showAccount.innerHTML = account;
+  } else {
+    web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
   }
+  
 
 
 
 
-l
+
+const address = "0x1a0E6664D47218F2a31Ea3220A834EDF98da5884"; //contract address
+let minter_address = "0xb14969b2eCA9733150fFF0C2e9b45844A25e371F"; // Minter address
+
+const contract = new web3.eth.Contract([
+{
+  "anonymous": false,
+  "inputs": [
+    {
+      "indexed": true,
+      "internalType": "address",
+      "name": "_owner",
+      "type": "address"
+    },
+    {
+      "indexed": true,
+      "internalType": "address",
+      "name": "_spender",
+      "type": "address"
+    },
+    {
+      "indexed": false,
+      "internalType": "uint256",
+      "name": "_value",
+      "type": "uint256"
+    }
+  ],
+  "name": "Approval",
+  "type": "event"
+},
+{
+  "anonymous": false,
+  "inputs": [
+    {
+      "indexed": true,
+      "internalType": "address",
+      "name": "_from",
+      "type": "address"
+    },
+    {
+      "indexed": true,
+      "internalType": "address",
+      "name": "_to",
+      "type": "address"
+    },
+    {
+      "indexed": false,
+      "internalType": "uint256",
+      "name": "_value",
+      "type": "uint256"
+    }
+  ],
+  "name": "Transfer",
+  "type": "event"
+},
+{
+  "inputs": [
+    {
+      "internalType": "address",
+      "name": "_owner",
+      "type": "address"
+    },
+    {
+      "internalType": "address",
+      "name": "_spender",
+      "type": "address"
+    }
+  ],
+  "name": "allowance",
+  "outputs": [
+    {
+      "internalType": "uint256",
+      "name": "remaining",
+      "type": "uint256"
+    }
+  ],
+  "stateMutability": "view",
+  "type": "function"
+},
+{
+  "inputs": [
+    {
+      "internalType": "address",
+      "name": "_spender",
+      "type": "address"
+    },
+    {
+      "internalType": "uint256",
+      "name": "_value",
+      "type": "uint256"
+    }
+  ],
+  "name": "approve",
+  "outputs": [
+    {
+      "internalType": "bool",
+      "name": "success",
+      "type": "bool"
+    }
+  ],
+  "stateMutability": "nonpayable",
+  "type": "function"
+},
+{
+  "inputs": [
+    {
+      "internalType": "address",
+      "name": "_owner",
+      "type": "address"
+    }
+  ],
+  "name": "balanceOf",
+  "outputs": [
+    {
+      "internalType": "uint256",
+      "name": "balance",
+      "type": "uint256"
+    }
+  ],
+  "stateMutability": "view",
+  "type": "function"
+},
+{
+  "inputs": [],
+  "name": "decimals",
+  "outputs": [
+    {
+      "internalType": "uint8",
+      "name": "",
+      "type": "uint8"
+    }
+  ],
+  "stateMutability": "view",
+  "type": "function"
+},
+{
+  "inputs": [],
+  "name": "name",
+  "outputs": [
+    {
+      "internalType": "string",
+      "name": "",
+      "type": "string"
+    }
+  ],
+  "stateMutability": "view",
+  "type": "function"
+},
+{
+  "inputs": [],
+  "name": "symbol",
+  "outputs": [
+    {
+      "internalType": "string",
+      "name": "",
+      "type": "string"
+    }
+  ],
+  "stateMutability": "view",
+  "type": "function"
+},
+{
+  "inputs": [],
+  "name": "totalSupply",
+  "outputs": [
+    {
+      "internalType": "uint256",
+      "name": "",
+      "type": "uint256"
+    }
+  ],
+  "stateMutability": "view",
+  "type": "function"
+},
+{
+  "inputs": [
+    {
+      "internalType": "address",
+      "name": "_to",
+      "type": "address"
+    },
+    {
+      "internalType": "uint256",
+      "name": "_value",
+      "type": "uint256"
+    }
+  ],
+  "name": "transfer",
+  "outputs": [
+    {
+      "internalType": "bool",
+      "name": "success",
+      "type": "bool"
+    }
+  ],
+  "stateMutability": "nonpayable",
+  "type": "function"
+},
+{
+  "inputs": [
+    {
+      "internalType": "address",
+      "name": "_from",
+      "type": "address"
+    },
+    {
+      "internalType": "address",
+      "name": "_to",
+      "type": "address"
+    },
+    {
+      "internalType": "uint256",
+      "name": "_value",
+      "type": "uint256"
+    }
+  ],
+  "name": "transferFrom",
+  "outputs": [
+    {
+      "internalType": "bool",
+      "name": "success",
+      "type": "bool"
+    }
+  ],
+  "stateMutability": "nonpayable",
+  "type": "function"
+}
+], address)
+
+console.log(contract);
+
+//interact with smart contract
+
+async function claim(amount) {
+  const accounts = await window.ethereum.request({method: 'eth_requestAccounts'});
+  const account = accounts[0];
+  const tx = await contract.methods.transferFrom(minter_address, account, amount).send({from:account});
+  
+}
+
+const form = document.getElementById("tokenBuy");
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log(form.input.value);
+    claim(form.input.value);
+})
+  
+//   async function getAccount() {
+//    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+//    const account = accounts[0];
+//    showAccount.innerHTML = account;
+//  }
+
+
+
+
